322a2c513c38d5639dc508e1f543db4cc33bc369
Shiv Shah <112986630+ShivShah202000@users.noreply.github.com>
2024-12-06 07:45:52 -0800

    Update next.config.mjs

--- next-app/next.config.mjs
+++ next-app/next.config.mjs
@@ -1,10 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    output: 'standalone',
-    images: {
-        domains: ['images.unsplash.com','i.ytimg.com'],
-      },
+  reactStrictMode: false,
+  output: 'standalone',
+  images: {
+    domains: ['images.unsplash.com', 'i.ytimg.com'],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Don't attempt to import these Node.js modules on the client side
+      config.resolve.fallback = {
+        fs: false,
+        'fs/promises': false,
+        child_process: false,
+        async_hooks: false,
+        net: false,
+        tls: false,
+        path: false,
+        crypto: false,
+      };
+    }
+    return config;
+  },
+  transpilePackages: ['@prisma/client'],
 };
 
 export default nextConfig;
